d7c795f3da841711568665506a9b6f4c805d17f4
mouhamed <coulibalymouhamed27@gmail.com>
2022-12-29 22:17:09 +0000

    depot:first add project

---
+++ daily_challege.js
@@ -0,0 +1,15 @@
+let n = 6
+let chaine = ''
+for (let i = 0; i < n; i++) {
+  chaine = chaine + "*"
+  console.log(chaine)
+}
+
+let nb = 2
+let chaineb = ""
+for (let i = 0; i <= nb - 1; i++) {
+  for (let j = 0; j <= nb; j++) {
+    chaineb = chaineb + "*"
+    console.log(chaineb);
+  }
+}
